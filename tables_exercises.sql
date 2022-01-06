30a52434f6931136647c167ea72a524e6821d6b3
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-06 16:22:16 -0600

    SQL Tables Exercises

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,6 +1,13 @@
-SHOW DATABASES;
-USE `employees`;
+SHOW DATABASES; # displays all databases in the server
+USE `employees`; #selects the employees database
 DESCRIBE employees; # describe employees database
 SHOW TABLES; # Show all tables
 DESCRIBE `employees`; # employees table int, date, varchar, enum, date
 DESCRIBE `salaries`; # contains both numeric and date
+SELECT employees.`first_name`, employees.`last_name` FROM employees; # contains string name
+SELECT employees.`birth_date`, employees.`hire_date` FROM employees; # contains date types
+DESCRIBE `departments`;
+SELECT * FROM departments;
+SELECT * FROM employees;
+
+SHOW CREATE TABLE `dept_manager`; #Query to create the dept_table
