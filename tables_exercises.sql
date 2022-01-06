20be84d24d9df5bdcdcc2f058e64ea12cb51d434
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-06 15:58:47 -0600

    SQL Tables Exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,6 @@
+SHOW DATABASES;
+USE `employees`;
+DESCRIBE employees; # describe employees database
+SHOW TABLES; # Show all tables
+DESCRIBE `employees`; # employees table int, date, varchar, enum, date
+DESCRIBE `salaries`; # contains both numeric and date
