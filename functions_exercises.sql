32e784c18d5f58a8403923bf9f65c8421389764c
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-07 16:23:58 -0600

    SQL Functions Exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,4 +1,8 @@
 #SQL FUNCTIONS EXERCISES
+SHOW DATABASES;
+
+SHOW TABLES;
+SELECT * FROM employees;
 
 
 -- 1. Copy the ORDER BY exercise AND save it AS functions_exercises.sql.
@@ -8,12 +12,15 @@
 
 -- 2.  WRITE a QUERY TO TO find ALL employees whose LAST NAME STARTS AND ENDS WITH 'E'. USE concat() TO combine their FIRST AND LAST NAME together AS a single COLUMN named full_name.
 
-
+SELECT CONCAT(first_name, ' ',last_name) AS full_name  FROM employees;
 
 -- 3. CONVERT the NAMES produced IN your LAST QUERY TO ALL uppercase.
+SELECT UPPER(CONCAT(first_name, ' ',last_name)) AS full_name  FROM employees ;
 
 
 -- 4. Find ALL employees hired IN the 90s AND born ON Christmas. USE datediff() FUNCTION TO find how many days they have been working AT the company (Hint: You will also need TO USE NOW() OR CURDATE()),
+SELECT first_name, last_name FROM employees WHERE (hire_date LIKE '%-12-25%') AND (birth_date BETWEEN '1990-01-01' AND '1999-12-31');
+
 
 -- 5. Find the smallest AND largest current salary FROM the salaries table.
 
