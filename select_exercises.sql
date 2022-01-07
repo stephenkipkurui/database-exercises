f2a48f7daa5a43a6efe329ef8d73a779d6f23013
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-07 11:06:56 -0600

    Select Statements Exercises on SQL

--- select_exercises.sql
+++ select_exercises.sql
@@ -14,7 +14,7 @@ SELECT * FROM albums;
 
 	 #b. Unique artists names in albums table: 31
 	 
-SELECT COUNT(DISTINCT name) FROM albums as unique_names_count;
+SELECT COUNT(DISTINCT NAME) FROM albums AS unique_names_count;
 
 	 #c.  Primary Key: id 
 	 #d. Oldest release date for any album in albums table: 1967
@@ -23,25 +23,26 @@ SELECT MIN(`release_date`) FROM albums;
 
 
 #4. a. Name of album by Pink Floyd: The Dark Side of the Moon & The Wall
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT NAME FROM albums WHERE artist = 'Pink Floyd';
 
 #b: Year SGT Pepper was released: 1967
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club band';
+SELECT release_date FROM albums WHERE NAME = 'Sgt. Pepper\'s Lonely Hearts Club band';
 
 #c. Genre of album Nevermind: Grudge, Alternative rock
-SELECT genre FROM albums WHERE name = "Nevermind";
+SELECT genre FROM albums WHERE NAME = "Nevermind";
 
 #d. Album released in 1990s
-SELECT name FROM albums WHERE `release_date` >= 1990 and `release_date` <= 1999;
+SELECT NAME FROM albums WHERE `release_date` >= 1990 AND `release_date` <= 1999;
 
-SELECt * FROM albums WHERE `release_date` BETWEEN 1990 AND 1999;
+SELECT * FROM albums WHERE `release_date` BETWEEN 1990 AND 1999;
 
 #e. Albums less than 20 million certified sales:
-SELECT name FROM albums WHERE sales <= 20.0;
+SELECT NAME FROM albums WHERE sales <= 20.0;
 
 #f. All albums with 'Rock'
 
-SELECT name FROM albums WHERE genre LIKE '%Rock%';
+SELECT NAME FROM albums WHERE genre LIKE '%Rock%';
+
 
 
 
