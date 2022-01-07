6f6937086f13c0b1f2c5afb439733f3eb9121069
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-07 14:28:02 -0600

    Order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,16 @@
+#SQL EXERCISE ORDER BY
+
+
+#1.Create a NEW FILE named order_by_exercises.sql AND copy IN the contents of your exercise FROM the previous lesson.
+
+#2. Find ALL employees WITH FIRST NAMES 'Irena', 'Vidya', OR 'Maya', AND order your results returned BY FIRST name. IN your comments, answer: What was the FIRST AND LAST NAME IN the FIRST ROW of the results? What was the FIRST AND LAST NAME of the LAST person IN the TABLE?
+
+#3. Find ALL employees WITH FIRST NAMES 'Irena', 'Vidya', OR 'Maya', AND order your results returned BY FIRST NAME AND THEN LAST name. IN your comments, answer: What was the FIRST AND LAST NAME IN the FIRST ROW of the results? What was the FIRST AND LAST NAME of the LAST person IN the TABLE?
+
+#4. Find ALL employees WITH FIRST NAMES 'Irena', 'Vidya', OR 'Maya', AND order your results returned BY LAST NAME AND THEN FIRST name. IN your comments, answer: What was the FIRST AND LAST NAME IN the FIRST ROW of the results? What was the FIRST AND LAST NAME of the LAST person IN the TABLE?
+
+#5. WRITE a QUERY TO TO find ALL employees whose LAST NAME STARTS AND ENDS WITH 'E'. Sort the results BY their employee number. Enter a COMMENT WITH the number of employees returned, the FIRST employee number AND their FIRST AND LAST NAME, AND the LAST employee number WITH their FIRST AND LAST name.
+
+#6. WRITE a QUERY TO TO find ALL employees whose LAST NAME STARTS AND ENDS WITH 'E'. Sort the results BY their hire DATE, so that the newest employees are listed first. Enter a COMMENT WITH the number of employees returned, the NAME of the newest employee, AND the NAME of the oldest employee.
+
+#7. Find ALL employees hired IN the 90s AND born ON Christmas. Sort the results so that the oldest employee who was hired LAST IS the FIRST result. Enter a COMMENT WITH the number of employees returned, the NAME of the oldest employee who was hired LAST, AND the NAME of the youngest employee who was hired first. 
