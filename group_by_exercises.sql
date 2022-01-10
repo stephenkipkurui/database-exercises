7339f35bab961e14b75db821bb7ee1634c6e3282
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-10 16:01:17 -0600

    Group By SQL Exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,22 +12,24 @@ DESCRIBE TABLE titles;
 SELECT * FROM employees LIMIT 4;
 SELECT DISTINCT title AS distinct_titles  FROM titles; # 7 titles
 
+SELECT title FROM titles GROUP BY title;
+
 -- 3. WRITE a QUERY TO find a LIST of ALL UNIQUE LAST NAMES of ALL employees that START AND END WITH 'E' USING GROUP BY.
 #SELECT DISTINCT	last_name FROM employees WHERE last_name LIKE 'E%E';
 SELECT last_name FROM employees WHERE last_name LIKE 'E%E' GROUP BY last_name;
 
--- 4. WRITE a QUERY TO find ALL UNIQUE combinations of FIRST AND LAST NAMES of ALL employees whose LAST NAMES START AND END WITH 'E'.**********************************
+-- 4. WRITE a QUERY TO find ALL UNIQUE combinations of FIRST AND LAST NAMES of ALL employees whose LAST NAMES START AND END WITH 'E'.
 SELECT CONCAT(first_name, last_name) AS full_name FROM employees WHERE last_name  LIKE 'E%E' GROUP BY full_name;
 
--- 5. WRITE a QUERY TO find the UNIQUE LAST NAMES WITH a 'q' but NOT 'qu'. Include those NAMES IN a COMMENT IN your SQL code. ********************************** Not using GROUP BY
-SELECT last_name FROM employees WHERE  last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+-- 5. WRITE a QUERY TO find the UNIQUE LAST NAMES WITH a 'q' but NOT 'qu'. Include those NAMES IN a COMMENT IN your SQL code.
+SELECT last_name FROM employees WHERE  last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
--- 6. ADD a COUNT() TO your results (the QUERY above) TO find the number of employees WITH the same LAST name. # [ 547 RECORDS ]
-SELECT COUNT(last_name) FROM employees WHERE  last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'; 
+-- 6. ADD a COUNT() TO your results (the QUERY above) TO find the number of employees WITH the same LAST name. #**************************
+SELECT last_name, COUNT(*) FROM employees WHERE  last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'; 
 
 
 -- 7 Find ALL ALL employees WITH FIRST NAMES 'Irena', 'Vidya', OR 'Maya'. USE COUNT(*) AND GROUP BY TO find the number of employees FOR EACH gender WITH those names. ***********************
-SELECT first_name, COUNT(gender) AS gender_count FROM employees GROUP BY first_name HAVING first_name = 'Irene' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT first_name, gender, COUNT(*) AS gender_count FROM employees WHERE first_name IN ('Irene', 'Vidya', 'Maya') GROUP BY first_name;
 
 
 -- 8 USING your QUERY that generates a username FOR ALL of the employees, generate a count employees FOR EACH UNIQUE username. Are there ANY DUPLICATE usernames? BONUS: How many DUPLICATE usernames are there?*************************
@@ -40,7 +42,7 @@ SELECT LOWER(
 		'_',
 		SUBSTR(birth_date,6, 2),
 		SUBSTR(birth_date,3, 2)
-	)) AS username, COUNT(*) FROM employees GROUP BY username ORDER BY COUNT(*) DESC;
+	)) AS username, COUNT(*) AS num_shared FROM employees GROUP BY username ORDER BY num_shared DESC;
 	
 	
 
@@ -53,16 +55,20 @@ SELECT LOWER(
 SELECT * FROM salaries LIMIT 5;
 SELECT * FROM employees LIMIT 4;
 
-SELECT salary, AVG(salary) AS average_salaries FROM salaries GROUP BY salary;
+SELECT emp_no, AVG(salary) AS average_salaries FROM salaries GROUP BY emp_no;
 
 
---      b). USING the dept_emp TABLE, count how many current employees WORK IN EACH department. The QUERY result should SHOW 9 ROWS, ONE 				FOR EACH department AND the employee count.
+--      b). USING the dept_emp TABLE, count how many current employees WORK IN EACH department. The QUERY result should SHOW 9 ROWS, ONE 	FOR EACH department AND the employee count.*****************************
 
-SELECT COUNT(*) AS employee_per_dept, dept_emp.`emp_no`, employees.`first_name`, employees.`last_name` FROM dept_emp INNER JOIN employees ON dept_emp.`emp_no` = employees.`emp_no` GROUP BY employees.`emp_no`;
+/* SELECT COUNT(*) AS employee_per_dept, dept_emp.`emp_no`, employees.`first_name`, employees.`last_name` FROM dept_emp INNER JOIN employees ON dept_emp.`emp_no` = employees.`emp_no` GROUP BY employees.`emp_no`; */
+
+SELECT dept_no, COUNT(*) FROM dept_emp WHERE to_date > NOW() GROUP BY emp_no;
 
 
 -- 		c). Determine how many different salaries EACH employee has had. This includes BOTH historic AND current.***********************
 SELECT COUNT(*) AS employee_salary_count, employees.`first_name`, employees.`last_name`, salaries.`salary` FROM employees INNER JOIN salaries ON salaries.`emp_no` = employees.`emp_no`GROUP BY employees.`emp_no`;
+
+
 
 
 -- 		d). Find the maximum salary FOR EACH employee.
@@ -73,8 +79,8 @@ SELECT MAX(salaries.`emp_no`) AS max_salary, employees.`first_name`, employees.`
 -- 		e). Find the minimum salary FOR EACH employee.
 SELECT MIN(salaries.`emp_no`) AS min_salary, employees.`first_name`, employees.`last_name` FROM employees INNER JOIN salaries ON salaries.`emp_no` = employees.`emp_no` GROUP BY employees.`emp_no`;
 
--- 		f). Find the standard deviation of salaries FOR EACH employee.
-SELECT STDDEV(salaries.`emp_no`) AS std_salary, employees.`first_name`, employees.`last_name` FROM employees INNER JOIN salaries ON salaries.`emp_no` = employees.`emp_no` GROUP BY employees.`emp_no`;
+-- 		f). Find the standard deviation of salaries FOR EACH employee.**************************
+SELECT STD(salaries.`emp_no`) AS std_salary, employees.`first_name`, employees.`last_name` FROM employees INNER JOIN salaries ON salaries.`emp_no` = employees.`emp_no` GROUP BY employees.`emp_no`;
 
 -- 		g). Now find the max salary FOR EACH employee WHERE that max salary IS greater THAN $150,000.
 SELECT MAX(salaries.`emp_no`) AS max_salary, employees.`first_name`, employees.`last_name` FROM employees INNER JOIN salaries ON salaries.`emp_no` = employees.`emp_no` GROUP BY employees.`emp_no` HAVING max_salary > 150000;
