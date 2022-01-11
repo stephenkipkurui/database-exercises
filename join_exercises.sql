6005a11505963590b309d1a879232a3d7f4a2817
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-11 12:03:36 -0600

    join SQL exercises

---
+++ join_exercises.sql
@@ -0,0 +1,11 @@
+-- 1). Use the employees database.
+SHOW DATABASES;
+USE employees; 
+SHOW TABLES;
+-- 2). Using the example in the Associative Table Joins section as a guide, write a query that shows each department along 
+-- with the name of the current manager for that department.
+
+
+
+-- 3). Find the name of all departments currently managed by women.
+
