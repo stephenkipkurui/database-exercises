9b057495f2f8f5e728f52854f67f168368b2a380
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-13 12:57:16 -0600

    SQL Case Exercises

--- case_exercises.sql
+++ case_exercises.sql
@@ -11,7 +11,7 @@ SELECT * FROM dept_emp LIMIT 5;
 
 -- 1).  WRITE a QUERY that RETURNS ALL employees, their department number, their START DATE, their END DATE, AND a NEW COLUMN 'is_current_employee' that IS a 1 IF the employee IS still WITH the company AND 0 IF not.
 
-SELECT CONCAT(`first_name`, ' ',`last_name`), `dept_no`, from_date AS 'FROM DATE', `to_date` AS 'END DATE',  
+SELECT CONCAT(`first_name`, ' ',`last_name`) AS 'Full Name', `dept_no` AS 'Department Number', from_date AS 'FROM DATE', `to_date` AS 'END DATE',  
 
   CASE 
   
@@ -29,7 +29,7 @@ JOIN employees USING (`emp_no`);
   
 -- 2). WRITE a QUERY that RETURNS ALL employee NAMES (previous AND current), AND a NEW COLUMN 'alpha_group' that RETURNS 'A-H', 'I-Q', OR 'R-Z' depending ON the FIRST letter of their LAST name.
 
-SELECT `first_name` AS 'First Name', `last_name` AS 'Last Name',
+SELECT `first_name` AS 'FIRST NAME', `last_name` AS 'LAST NAME',
 
   CASE 
   
@@ -39,7 +39,6 @@ SELECT `first_name` AS 'First Name', `last_name` AS 'Last Name',
     
     WHEN UPPER(SUBSTR(`last_name`, 1,1)) BETWEEN  'R' AND 'Z' THEN 'R-Z'
     
-    
   END AS 'alpha_group'
   
 FROM `employees`
@@ -47,11 +46,37 @@ FROM `employees`
 JOIN `dept_emp` USING (`emp_no`);
 
 
+-- 3). How many employees (current OR previous) were born IN EACH decade?
+
+SELECT 
+
+  birth_date, CONCAT(first_name,' ',last_name) AS 'FULL NAME',
+
+  CASE birth_date
+  
+    WHEN birth_date <  1900-01-01 THEN 'You were born BEFORE the 19000s '
+   
+    WHEN birth_date BETWEEN '1900-01-01' AND '1910-12-31' THEN 'You were born IN the 1900s'
+    WHEN birth_date BETWEEN '1910-01-01' AND '1920-12-31' THEN 'You were born BETWEEN the 1910-1920'
+    WHEN birth_date BETWEEN '1930-01-01' AND '1940-12-31' THEN 'You were born BETWEEN the 1910-1920'
+    WHEN birth_date BETWEEN '1950-01-01' AND '1960-12-31' THEN 'You were born BETWEEN the 1910-1920'
+    WHEN birth_date BETWEEN '1970-01-01' AND '1980-12-31' THEN 'You were born BETWEEN the 1910-1920'
+    WHEN birth_date BETWEEN '1990-01-01' AND '2000-12-31' THEN 'You were born BETWEEN the 1910-1920'
+
+  
+  
+  END AS 'Employees Count BY Decade'
+  
+  
+FROM employees
+
+GROUP BY birth_date;
+  
+  
+  
+    
 
 
-
-
--- How many employees (current OR previous) were born IN EACH decade?
 
 
 -- What IS the current average salary FOR EACH of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
