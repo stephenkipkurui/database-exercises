a0923f48a4c521263d489b48564c38b7d1bd0bed
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-13 17:23:25 -0600

    Temporary Table Exercises

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -10,23 +10,44 @@ SHOW TABLES;
 
 CREATE TEMPORARY TABLE employees_with_departments(col1 INT UNSIGNED NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL , dept_name VARCHAR(50) NOT NULL );
 
-
 -- a).  ADD a COLUMN named full_name TO this table. It should be a VARCHAR whose length IS the sum of the lengths of the FIRST NAME AND LAST NAME COLUMNS
 ALTER TABLE employees_with_departments ADD full_name VARCHAR(100);
 
 -- b).  UPDATE the TABLE so that FULL NAME COLUMN CONTAINS the correct DATA
+SELECT * FROM employees_with_departments;
+
+DESCRIBE employees_with_departments;
 
 
+UPDATE TABLE employees_with_departments SET full_name = CONCAT('first_name', ' ','last_name');
 
 -- c).  Remove the first_name AND last_name COLUMNS FROM the table.
+ALTER TABLE employees_with_departments DROP COLUMN full_name;
+
+
 -- d).  What IS another way you could have ended up WITH this same TABLE?
 
-
 /* 
- 2). CREATE a TEMPORARY TABLE based ON the payment TABLE FROM the sakila database. 
+ 2). CREATE a TEMPORARY TABLE based ON the payment TABLE FROM the sakila database. */ 
+USE sakila;
 
- WRITE the SQL necessary TO transform the amount COLUMN such that it IS stored AS an INTEGER representing the number of cents of the payment. FOR example, 1.99 should become 199.*/
+#DESCRIBE TABLE payment;
+ 
+CREATE TEMPORARY TABLE temp_payment AS ( SELECT * FROM sakila.payment ); 
 
+DESCRIBE temp_payment;
+
+ # WRITE the SQL necessary TO transform the amount COLUMN such that it IS stored AS an INTEGER representing the number of cents of the payment. FOR example, 1.99 should become 199.*/
+SELECT * FROM temp_payment LIMIT 5;
+
+DESCRIBE temp_payment;
+
+
+UPDATE temp_payment SET amount = CAST(amount AS INT); #*****************NEED REVIEW****************
 
 
 /* 3).  Find OUT how the current average pay IN EACH department compares TO the overall, historical average pay. IN order TO make the comparison easier, you should USE the Z-score FOR salaries. IN terms of salary, what IS the best department RIGHT now TO WORK FOR? The worst? */
+
+
+
+
