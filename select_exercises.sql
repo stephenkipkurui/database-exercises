1167fe6e56b758cc246a2cd338a30dbc4b481f5b
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-14 16:45:06 -0600

    SQL Select Exercises COMPLETED

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,47 +1,52 @@
-# Basic Statements SQL Exercises
+# BASIC STATEMENTS EXERCISES 
 
-SHOW DATABASES;
+-- 1). CREATE a NEW FILE called select_exercises.sql. Store your CODE FOR this exercise IN that file. You should be testing your CODE IN Sequel Ace AS you go.
+# Completed
+
+-- 2). USE the albums_db database.
 USE `albums_db`;
 
-SHOW TABLES IN `albums_db`;
+-- 3). Explore the structure of the albums table.
 DESCRIBE TABLE albums;
 
-#1. Created select_exercises.sql
-#2. Selected albums_db
-#3. Structure of album:  
-		#a. Rows: 31
-SELECT * FROM albums;
-
-	 #b. Unique artists names in albums table: 31
-	 
-SELECT COUNT(DISTINCT NAME) FROM albums AS unique_names_count;
-
-	 #c.  Primary Key: id 
-	 #d. Oldest release date for any album in albums table: 1967
-	 
-SELECT MIN(`release_date`) FROM albums;
+-- a. How many ROWS are IN the albums TABLE? #31
+SELECT COUNT(*) AS 'Record Count' FROM albums;
 
 
-#4. a. Name of album by Pink Floyd: The Dark Side of the Moon & The Wall
+-- b. How many UNIQUE artist NAMES are IN the albums TABLE?
+SELECT COUNT(DISTINCT(artist)) AS 'Distinct Artists'  FROM albums; #23
+
+
+-- c. What IS the PRIMARY KEY FOR the albums TABLE?
+DESCRIBE albums; # 'id' field is the PRIMARY KEY
+
+-- d. What IS the oldest RELEASE DATE FOR ANY album IN the albums TABLE? What IS the most recent RELEASE DATE?
+SELECT MIN(`release_date`) AS 'Oldest Release Date' FROM albums; #1967
+
+
+-- 4). WRITE queries TO find the following information:
+
+-- a. The NAME of ALL albums BY Pink Floyd
 SELECT NAME FROM albums WHERE artist = 'Pink Floyd';
 
-#b: Year SGT Pepper was released: 1967
-SELECT release_date FROM albums WHERE NAME = 'Sgt. Pepper\'s Lonely Hearts Club band';
+-- b. The YEAR Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date AS 'Sgt Pepper Release Date' FROM albums WHERE NAME = 'Sgt. Pepper\'s Lonely Hearts Club band';
 
-#c. Genre of album Nevermind: Grudge, Alternative rock
-SELECT genre FROM albums WHERE NAME = "Nevermind";
+-- c. The genre for the album Nevermind
+SELECT genre AS 'Nevermind Album' FROM albums WHERE NAME = "Nevermind";
 
-#d. Album released in 1990s
-SELECT NAME FROM albums WHERE `release_date` >= 1990 AND `release_date` <= 1999;
+-- d. Which albums were released IN the 1990s
+SELECT NAME AS 'Albums Released in 1990s' FROM albums WHERE `release_date` >= 1990 AND `release_date` <= 1999;
 
-SELECT * FROM albums WHERE `release_date` BETWEEN 1990 AND 1999;
+SELECT NAME, release_date, artist FROM albums WHERE `release_date` BETWEEN 1990 AND 1999;
 
-#e. Albums less than 20 million certified sales:
-SELECT NAME FROM albums WHERE sales <= 20.0;
 
-#f. All albums with 'Rock'
+-- e. Which albums had LESS THAN 20 million certified sales
+SELECT NAME AS 'Albums with Less than 20 million Sales' FROM albums WHERE sales <= 20.0;
 
-SELECT NAME FROM albums WHERE genre LIKE '%Rock%';
+-- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT NAME AS 'Rock Genre Albums' FROM albums WHERE genre LIKE '%Rock%';
+
 
 
 
