4270abb62e8070206d102bbdf35b1c01fbbfefbc
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-14 10:08:06 -0600

    Temporary Table Exercises

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -8,7 +8,7 @@ SHOW TABLES;
 
 /* 1). USING the example FROM the lesson, CREATE a TEMPORARY TABLE called employees_with_departments that CONTAINS first_name, last_name, AND dept_name FOR employees currently WITH that department. Be absolutely sure TO CREATE this TABLE ON your own database. IF you see "Access denied for user ...", it means that the QUERY was attempting TO WRITE a NEW TABLE TO a DATABASE that you can only read. */
 
-CREATE TEMPORARY TABLE employees_with_departments(col1 INT UNSIGNED NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL , dept_name VARCHAR(50) NOT NULL );
+CREATE TEMPORARY TABLE employees_with_departments(first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL , dept_name VARCHAR(50) NOT NULL );
 
 -- a).  ADD a COLUMN named full_name TO this table. It should be a VARCHAR whose length IS the sum of the lengths of the FIRST NAME AND LAST NAME COLUMNS
 ALTER TABLE employees_with_departments ADD full_name VARCHAR(100);
@@ -29,11 +29,12 @@ ALTER TABLE employees_with_departments DROP COLUMN full_name;
 
 /* 
  2). CREATE a TEMPORARY TABLE based ON the payment TABLE FROM the sakila database. */ 
+ 
 USE sakila;
 
 #DESCRIBE TABLE payment;
  
-CREATE TEMPORARY TABLE temp_payment AS ( SELECT * FROM sakila.payment ); 
+CREATE TEMPORARY TABLE temp_payment AS (SELECT * FROM sakila.payment); 
 
 DESCRIBE temp_payment;
 
@@ -43,11 +44,28 @@ SELECT * FROM temp_payment LIMIT 5;
 DESCRIBE temp_payment;
 
 
-UPDATE temp_payment SET amount = CAST(amount AS INT); #*****************NEED REVIEW****************
+UPDATE temp_payment SET amount = (
+
+
+SELECT 
+
+  amount, 
+  
+    CASE amount
+    
+      WHEN amount LIKE '%.%' THEN CONCAT(SUBSTR(amount,1,1), SUBSTR(amount,3,4))
+      
+    END AS new_amount
+    
+FROM temp_payment
+
+
+); #*****************NEED REVIEW****************
 
 
 /* 3).  Find OUT how the current average pay IN EACH department compares TO the overall, historical average pay. IN order TO make the comparison easier, you should USE the Z-score FOR salaries. IN terms of salary, what IS the best department RIGHT now TO WORK FOR? The worst? */
+SELECT 
 
-
+  
 
 
