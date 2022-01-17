0387a08704d1e75a4cc74e8ab471b6a0155cf327
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-17 00:26:39 -0600

    SQL Teporary Tables Exercises COMPLETED

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -30,7 +30,7 @@ ALTER TABLE employees_with_departments DROP COLUMN full_name;
 /* 
  2). CREATE a TEMPORARY TABLE based ON the payment TABLE FROM the sakila database. */ 
  
-USE sakila;
+#USE sakila;
 
 #DESCRIBE TABLE payment;
  
@@ -39,28 +39,15 @@ CREATE TEMPORARY TABLE temp_payment AS (SELECT * FROM sakila.payment);
 DESCRIBE temp_payment;
 
  # WRITE the SQL necessary TO transform the amount COLUMN such that it IS stored AS an INTEGER representing the number of cents of the payment. FOR example, 1.99 should become 199.*/
-SELECT * FROM temp_payment LIMIT 5;
+DROP TABLE IF EXISTS temp_payments;
 
-DESCRIBE temp_payment;
+CREATE TEMPORARY TABLE temp_payment 
 
+AS (
 
-UPDATE temp_payment SET amount = (amount * 100);
-
-/* UPDATE temp_payment SET amount = (
-
-SELECT 
-
-  amount, 
-  
-    CASE amount
+    SELECT payment_id, customer_id, staff_id, rental_id, amount * 100, payment_date, last_update 
     
-      WHEN amount LIKE '%.%' THEN CONCAT(SUBSTR(amount,1,1), SUBSTR(amount,3,4))
-      
-    END AS new_amount
-    
-FROM temp_payment
-
-); */ #*****************NEED REVIEW****************
+    FROM sakila.payment);
 
 
 /* 3).  Find OUT how the current average pay IN EACH department compares TO the overall, historical average pay. IN order TO make the comparison easier, you should USE the Z-score FOR salaries. IN terms of salary, what IS the best department RIGHT now TO WORK FOR? The worst? */
@@ -74,7 +61,50 @@ SELECT * FROM salaries LIMIT 5;
 SELECT * FROM departments LIMIT 5;
 
 
-# Current Average Salary
+SELECT AVG(salary) AS average_salary, STD(salary) AS standard_dev_salary
+
+FROM employees.salaries;
+
+CREATE TEMPORARY TABLE current_info AS (
+
+SELECT dept_name, AVG(salary) AS department_current_average
+
+    FROM employees.salaries
+      
+      JOIN employees.dept_emp USING(emp_no)
+      
+      JOIN employees.departments USING(dept_no)
+    
+        WHERE employees.dept_emp.to_date > curdate()
+    
+    AND employees.salaries.to_date > curdate()
+    
+GROUP BY dept_name
+);
+
+-- Create columns to hold the average salary, std, and the zscore
+ALTER TABLE current_info ADD average FLOAT(10,2);
+ALTER TABLE current_info ADD standard_deviation FLOAT(10,2);
+ALTER TABLE current_info ADD zscore FLOAT(10,2);
+
+UPDATE current_info SET average = 63810;
+UPDATE current_info SET standard_deviation = 16904;
+
+
+-- z_score  = (avg(x) - x) / std(x) */
+UPDATE current_info 
+SET zscore = (department_current_average - historic_avg) / historic_std;
+
+SELECT * FROM current_info
+ORDER BY zscore DESC;
+
+
+
+
+
+
+-- [----------------Start__________________________]
+/* # Current Average Salary
 SELECT d.dept_name, AVG(s.salary) AS 'Average Current Department Salary'
 
 FROM departments d 
@@ -99,14 +129,14 @@ JOIN salaries s USING(emp_no)
 
 WHERE s.to_date != '9999-01-01'
 
-GROUP BY dept_name; 
+GROUP BY dept_name;  */
+-- [------------------END---------------------------]
 
 
 
+# SUMMARY:
 
-
-
-  
+-- ********Need more practice with subqueries and undestanding z-score concepts*******
 
 
 
