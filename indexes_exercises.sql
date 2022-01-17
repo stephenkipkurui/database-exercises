bcdfd2cdff945ddb0017952c4d2fb3adfc950356
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-16 21:01:05 -0600

    SQL Idexes Exercises COMPLETED

---
+++ indexes_exercises.sql
@@ -0,0 +1,32 @@
+-- INDEXES SQL EXERCISES
+
+USE employees;
+
+SHOW TABLES;
+
+DESCRIBE TABLE departments;
+-- Indexes & Keys: id
+ 
+
+DESCRIBE TABLE dept_emp;
+-- Indexes & Keys: id
+ 
+
+
+DESCRIBE TABLE dept_manager;
+-- Indexes & Keys: id
+
+
+DESCRIBE TABLE employees;
+-- Indexes & Keys: id
+
+
+DESCRIBE TABLE salaries;
+-- Indexes & Keys: id
+
+
+
+DESCRIBE TABLE titles;
+-- Indexes & Keys: id
+
+
