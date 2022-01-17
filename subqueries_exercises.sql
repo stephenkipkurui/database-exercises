3c4da2842bb254b7eb6501a7d6a3ccf319570d8a
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-17 00:05:13 -0600

    SQL Subqueries Exercises COMPLETED

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,3 +1,5 @@
+# SQL SUBQUERIES EXERCISES
+
 
 USE employees;
 SHOW TABLES;
@@ -5,6 +7,8 @@ SHOW TABLES;
 SELECT * FROM employees LIMIT 5;
 SELECT * FROM titles LIMIT 5;
 SELECT * FROM salaries LIMIT 5;
+SELECT * FROM departments LIMIT 5;
+SELECT * FROM dept_manager LIMIT 5;
 
 -- 1). Find ALL the current employees WITH the same hire DATE AS employee 101010 USING a sub-query.
 
@@ -36,11 +40,35 @@ WHERE emp_no IN
 (SELECT emp_no FROM salaries WHERE `to_date` < '9999-01-01');
 
 
--- 4). Find ALL the current department managers that are female. LIST their NAMES IN a COMMENT IN your code.
+-- 4). Find ALL the current department managers that are female. LIST their NAMES IN a COMMENT IN your code. [__________ Hilary Kambil, Leon DasSarma, Karsten Sigstan, Isamu Legleitner_________]
+
+SELECT * 
+
+FROM employees
+
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date = '9999-01-01'
+
+AND gender IN (SELECT gender FROM employees WHERE gender = 'F')
+
+);
+
 
 -- 5). Find ALL the employees who currently have a higher salary THAN the companies overall, historical average salary.
+SELECT * FROM employees 
+
+WHERE emp_no IN (
+
+SELECT emp_no FROM salaries WHERE salary > (SELECT  AVG(salary) FROM salaries WHERE to_date > CURDATE()) 
+
+AND to_date > now());
+
 
 -- 6). How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can USE a built IN FUNCTION TO calculate the standard deviation.) What percentage of ALL salaries IS this?
 
+# [************Expirienced difficulty- Need more practice with this question******************]
+
 -- Hint Number 1 You will likely USE a combination of different kinds of subqueries.
 -- Hint Number 2 Consider that the following CODE will produce the z score FOR current salaries.
+
+# [************Expirienced difficulty- Need more practice with this question******************]
+
