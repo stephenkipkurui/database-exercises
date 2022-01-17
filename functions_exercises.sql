e5cf6f3cf800f551d14c3d3a987bf29fc20509b7
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-01-16 19:23:24 -0600

    SQL Function Exercises  COMPLETED

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,8 +2,8 @@
 SHOW DATABASES;
 
 SHOW TABLES;
-SELECT * FROM employees;
-SELECT * FROM salaries;
+SELECT * FROM employees LIMIT 5;
+SELECT * FROM salaries LIMIT 5;
 
 
 -- 1. Copy the ORDER BY exercise AND save it AS functions_exercises.sql.
@@ -17,18 +17,26 @@ SELECT CONCAT(first_name, ' ',last_name) AS full_name  FROM employees;
 -- 3. CONVERT the NAMES produced IN your LAST QUERY TO ALL uppercase.
 SELECT UPPER(CONCAT(first_name, ' ',last_name)) AS full_name  FROM employees ;
 
-
 -- 4. Find ALL employees hired IN the 90s AND born ON Christmas. USE datediff() FUNCTION TO find how many days they have been working AT the company (Hint: You will also need TO USE NOW() OR CURDATE()),
-SELECT DATEDIFF(NOW() , hire_date) AS days_in_company FROM employees WHERE birth_date LIKE '%-12-25%' AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-
+SELECT CONCAT(first_name, ' ', last_name) 'Employee Name: ', DATEDIFF(NOW() , hire_date) AS days_in_company FROM employees WHERE birth_date LIKE '%-12-25%' AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 -- 5. Find the smallest AND largest current salary FROM the salaries table.
 SELECT MIN(salary) AS minimum_salary, MAX(salary)  AS maximum_salary FROM salaries; # max 
 
-
-
 -- 6. USE your knowledge of built IN SQL functions TO generate a username FOR ALL of the employees. A username should be ALL lowercase, AND consist of the FIRST CHARACTER of the employees FIRST NAME, the FIRST 4 characters of the employees LAST NAME, an underscore, the MONTH the employee was born, AND the LAST two digits of the YEAR that they were born. Below IS an example of what the FIRST 10 ROWS will look LIKE:
-SELECT * FROM  employees;
 
-SELECT CONCAT(CONCAT(SUBSTR(LOWER(first_name), 1,1), SUBSTR(LOWER(last_name), 1, 4)),'_') AS username, first_name, last_name, birth_date FROM employees;
+SELECT CONCAT(first_name, ' ', last_name),
 
+  LOWER(
+      
+      CONCAT(
+      
+          SUBSTR(first_name, 1, 1),
+          SUBSTR(last_name, 1, 4),
+          '_',
+          SUBSTR(birth_date, 6, 2),
+          SUBSTR(birth_date, 3, 2)
+          ) )
+  AS username 
+
+FROM employees;
